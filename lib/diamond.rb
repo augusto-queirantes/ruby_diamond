3026bb32498ab3b3a44ab0a9f9cc037adaf891fe
Augusto Queirantes <augusto_queirantes@outlook.com>
2021-01-12 17:39:54 -0300

    Add first functional version

--- lib/diamond.rb
+++ lib/diamond.rb
@@ -1,4 +1,69 @@
 class Diamond
   def build(input)
+    input_ascii_code = input.ord
+    input_ascii_index = input_ascii_code - first_letter_ascii_code
+    board_dimension = calculate_board_dimension(input_ascii_index)
+    current_letter_ascii_code = first_letter_ascii_code
+    still_different_than_input = true
+    response = ""
+
+    board_dimension.times do |index|
+      response += "#{build_line(current_letter_ascii_code, input_ascii_code)}\n"
+
+      if still_different_than_input
+        current_letter_ascii_code += 1
+      else
+        current_letter_ascii_code -= 1
+      end
+
+      still_different_than_input = false if current_letter_ascii_code == input_ascii_code
+    end
+
+    response
+  end
+
+  private
+
+  def build_line(current_letter_ascii_code, input_ascii_code)
+    extremities_underlines = build_extremities_underlines(current_letter_ascii_code, input_ascii_code)
+    letter_content = build_letter(current_letter_ascii_code, input_ascii_code)
+
+    "#{build_underlines(extremities_underlines)}#{letter_content}#{build_underlines(extremities_underlines)}"
+  end
+
+  def build_extremities_underlines(current_letter_ascii_code, input_ascii_code)
+    input_ascii_code - current_letter_ascii_code
+  end
+
+  def build_letter(current_letter_ascii_code, input_ascii_code)
+    inside_underlines = build_inside_underlines(current_letter_ascii_code - first_letter_ascii_code)
+
+    if current_letter_ascii_code == 65
+      current_letter_ascii_code.chr
+    else
+      "#{current_letter_ascii_code.chr}#{build_underlines(inside_underlines)}#{current_letter_ascii_code.chr}"
+    end
+  end
+
+  def calculate_board_dimension(input_ascii_index)
+    return 1 if input_ascii_index.zero?
+    return (input_ascii_index + (input_ascii_index + 1))
+  end
+
+  def build_inside_underlines(number)
+    return 0 if number.zero?
+    return (number + (number - 1))
+  end
+
+  def first_letter_ascii_code
+    "A".ord
+  end
+
+  def last_letter_ascii_code
+    "Z".ord
+  end
+
+  def build_underlines(underlines_number)
+    "_" * underlines_number
   end
 end
--- spec/diamond_spec.rb
+++ spec/diamond_spec.rb
@@ -6,21 +6,25 @@ RSpec.describe Diamond do
 
     context "when given an A character" do
       let(:input) { 'A' }
+
       it { is_expected.to eq("A\n") }
     end
 
     context "when given a B character" do
       let(:input) { 'B' }
+
       it { is_expected.to eq("_A_\nB_B\n_A_\n") }
     end
 
     context "when given a C character" do
       let(:input) { 'C' }
+
       it { is_expected.to eq("__A__\n_B_B_\nC___C\n_B_B_\n__A__\n") }
     end
 
     context "when given a J character" do
       let(:input) { 'J' }
+
       it { is_expected.to eq(<<~EXAMPLE) }
         _________A_________
         ________B_B________
