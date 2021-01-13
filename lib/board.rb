a2f0f33f424de2c3330ad0b511adad1e6c241a40
Augusto Queirantes <augusto_queirantes@outlook.com>
2021-01-13 06:36:18 -0300

    update calculate_dimension param name

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Board
-  def self.calculate_dimension(input_letter)
-    input_ascii_index = input_letter.ord - 'A'.ord
+  def self.calculate_dimension(input)
+    input_ascii_index = input.ord - 'A'.ord
 
     return 1 if input_ascii_index.zero?
 
