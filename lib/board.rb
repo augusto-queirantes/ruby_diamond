3715d77776b4cddd60bd9d9f53bbfa6f6c7e4e61
Augusto Queirantes <augusto_queirantes@outlook.com>
2021-01-13 01:02:43 -0300

    Add class to manage the moard

---
+++ lib/board.rb
@@ -0,0 +1,17 @@
+class Board
+  def initialize(input_letter)
+    @input_letter = input_letter
+  end
+
+  def calculate_dimension
+    input_ascii_index = input_letter.ord - "A".ord
+
+    return 1 if input_ascii_index.zero?
+
+    input_ascii_index + (input_ascii_index + 1)
+  end
+
+  private
+
+  attr_reader :input_letter
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe Board do
+  let(:instance) { described_class.new(input_letter) }
+
+  describe "#calculate_dimension" do
+    subject { instance.calculate_dimension }
+
+    context "when input letter is an A" do
+      let(:input_letter) { "A" }
+
+      it { is_expected.to eq(1) }
+    end
+
+    context "when input letter is an B" do
+      let(:input_letter) { "B" }
+
+      it { is_expected.to eq(3) }
+    end
+
+    context "when input letter is an C" do
+      let(:input_letter) { "C" }
+
+      it { is_expected.to eq(5) }
+    end
+
+    context "when input letter is an Z" do
+      let(:input_letter) { "Z" }
+
+      it { is_expected.to eq(51) }
+    end
+  end
+end
