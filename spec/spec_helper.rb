4c7f9fd9ff52310b2b7a7fa3a779f0e3d3360811
Augusto Queirantes <augusto_queirantes@outlook.com>
2021-01-13 01:03:27 -0300

    Update spec helper to be able to test new classes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require_relative '../lib/diamond.rb'
+require_relative '../lib/line.rb'
+require_relative '../lib/letter.rb'
+require_relative '../lib/board.rb'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
