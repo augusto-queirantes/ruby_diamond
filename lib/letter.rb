c9984bfbee1baff42ec359c1448ab34d6f84188f
Augusto Queirantes <augusto_queirantes@outlook.com>
2021-01-13 06:45:03 -0300

    Update next method param name

--- lib/letter.rb
+++ lib/letter.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
 class Letter
-  def initialize(input_letter)
-    @letter_ascii_code = 64
-    @input_letter_ascii_code = input_letter.ord
+  def initialize(input)
+    @first_letter_previous_ascii_code = 64
+    @input_ascii_code = input.ord
     @should_change_to_next_letter = true
   end
 
   def next
     if @should_change_to_next_letter
-      @letter_ascii_code += 1
+      @first_letter_previous_ascii_code += 1
     else
-      @letter_ascii_code -= 1
+      @first_letter_previous_ascii_code -= 1
     end
 
-    @should_change_to_next_letter = false if letter_ascii_code == input_letter_ascii_code
+    @should_change_to_next_letter = false if first_letter_previous_ascii_code == input_ascii_code
 
-    letter_ascii_code.chr
+    first_letter_previous_ascii_code.chr
   end
 
   private
 
-  attr_accessor :letter_ascii_code, :should_change_to_next_letter
-  attr_reader :input_letter_ascii_code
+  attr_accessor :first_letter_previous_ascii_code, :should_change_to_next_letter
+  attr_reader :input_ascii_code
 end
